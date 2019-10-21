5a130b0f1c650ab1c711f74a99f0a8cece92554b
catalinaboca <56110788+catalinaboca@users.noreply.github.com>
2019-10-21 12:33:25 +0300

    Primul MVC

---
+++ MVC/View.js
@@ -0,0 +1,35 @@
+class t_eq2_view
+{
+	events;
+	constructor()
+	{
+		document.getElementById("id_button").addEventListener("click",on_solve_click);
+	}
+	set_events_list(events)
+	{
+		this.events=events;
+	}
+	on_solve_click()
+	{
+		//ar trebui sa apeleze metoda on_solve din controller...dar nu putem direct
+		//generam un eveniment care apeleaza metoda on_sove stocata in lista 
+		events.call_event("solve_clicked_UAB");
+	}
+	get_a()
+	{
+	return document.getElementById("id_a").value;	
+	}
+	get_b()
+	{
+		return document.getElementById("id_b").value;
+	}
+	get_c()
+	{
+		return document.getElementById("id_c").value;
+	}
+	set_solutions_para(solutions)
+	{
+	document.getElementById("x1").innerHTML="x1="+solutions.x1_re+ "+"+solutions.x1_im+"i";
+	document.getElementById("x2").innerHTML="x2="+solutions.x2_re+"+"+solutions.x2_im+"i";
+	}
+}
