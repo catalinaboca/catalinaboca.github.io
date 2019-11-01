cf50a77e548e081b612fa2a773639568eb3e39d5
catalinaboca <56110788+catalinaboca@users.noreply.github.com>
2019-11-01 14:42:19 +0200

    Add files via upload

--- worker/desenare.js
+++ worker/desenare.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic").innerHTML = "Logic version=2019.11.1.2";
+document.getElementById("id_logic").innerHTML = "Logic version=2019.11.1.4";
 document.getElementById("id_start").addEventListener("click",start);
 document.getElementById("id_stop").addEventListener("click",stop);
 var timer_id;
