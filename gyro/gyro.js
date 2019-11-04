5ae1b22861399bf0b35ba85ea6c134b5023cc726
catalinaboca <56110788+catalinaboca@users.noreply.github.com>
2019-11-04 12:46:34 +0200

    Add files via upload

--- gyro/gyro.js
+++ gyro/gyro.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML="Logic=2019.11.4.3";
+document.getElementById("id_logic_version").innerHTML="Logic=2019.11.4.4";
 window.addEventListener("deviceorientation",on_gyro_data_uab);
 window.addEventListener("devicemotion",on_acc_data_uab);
 function deseneaza(unghi_x, unghi_y)
@@ -23,7 +23,7 @@ function on_gyro_data_uab(e)
 	document.getElementById("id_alpha").innerHTML=Math.round(e.alpha*100)/100;
 	document.getElementById("id_beta").innerHTML=Math.round(e.beta*100)/100;
 	document.getElementById("id_gamma").innerHTML=Math.round(e.gamma*100)/100;
-	deseneaza(e.beta,e.gamma);
+	deseneaza(e.gamma,e.beta);
 	
 }
 function on_acc_data_uab(e)
