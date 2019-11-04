ab01214b2d74c6c4fed863da59b9d7c595d2c6e4
catalinaboca <56110788+catalinaboca@users.noreply.github.com>
2019-11-04 12:39:18 +0200

    Add files via upload

--- gyro/gyro.js
+++ gyro/gyro.js
@@ -1,11 +1,29 @@
-document.getElementById("id_logic_version").innerHTML="Logic=2019.11.4.1";
+document.getElementById("id_logic_version").innerHTML="Logic=2019.11.4.3";
 window.addEventListener("deviceorientation",on_gyro_data_uab);
 window.addEventListener("devicemotion",on_acc_data_uab);
+function deseneaza(unghi_x, unghi_y)
+{
+	//obtinem context grafic 
+	var canvas=document.getElementById("id_canvas");
+	var context=canvas.getContext("2d");
+    var r=10;
+	
+	//construim o cale
+	context.beginPath();
+	//adaugam un cerc la cale
+	var x=unghi_x/90 * (canvas.width/2-r)+canvas.width/2;
+	var y=unghi_x/90 * (canvas.height/2-r)+canvas.height/2;
+	context.arc(x,y,z,0,2*Math.PI);
+	//desenam calea
+	context.stroke();
+	
+}
 function on_gyro_data_uab(e)
 {
 	document.getElementById("id_alpha").innerHTML=Math.round(e.alpha*100)/100;
 	document.getElementById("id_beta").innerHTML=Math.round(e.beta*100)/100;
 	document.getElementById("id_gamma").innerHTML=Math.round(e.gamma*100)/100;
+	deseneaza(e.beta,e.gamma);
 	
 }
 function on_acc_data_uab(e)
