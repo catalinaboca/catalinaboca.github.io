a5b3ec48697eb30d18d9322ee16962594cb9ae36
catalinaboca <56110788+catalinaboca@users.noreply.github.com>
2019-11-22 13:32:38 +0200

    Add files via upload

--- worker/calcul_prime.js
+++ worker/calcul_prime.js
@@ -1,3 +1,18 @@
+var ruleaza=false;
+
+onmessage=la_mesaj;
+function la_mesaj(e)
+{
+	if(e.data=="start")
+	{
+		ruleaza=true;
+	}
+	else
+		if(e.data=="stop")
+		{
+			ruleaza=false;
+		}
+}
 function is_prime(n)
 {
 	for(var i=2; i*i<=n;i++)
@@ -7,10 +22,24 @@ function is_prime(n)
 	}
 	return false;
 }
-for(var i=1E11;i<1E12;i++)
+function start_calcul(start_numar)
 {
+	if(ruleaza==false)
+		;
+	else{
+for(var i=sta_nmar;i<start_numar+100;i++)
 	if(is_prime(i))
-	{
+	
 		postMessage(i);
-	}
+		start_numar+=100;
 }
+if(start_numar<1E13)
+{
+	setTimeout(start_calcul,1,start_numar);
+}
+else
+{
+	postMessage("gata");
+}
+}
+start_calcul(1E11);
