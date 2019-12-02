0567a58b3eed48557e6b3006ef10b1a03bd2f51f
catalinaboca <56110788+catalinaboca@users.noreply.github.com>
2019-12-02 12:26:55 +0200

    Add files via upload

--- touch/touch.js
+++ touch/touch.js
@@ -1,9 +1,10 @@
-document.getElementById("id_logic").innerHTML="Logic 2019.12.02.1";
+document.getElementById("id_logic").innerHTML="Logic 2019.12.02.2";
 window.addEventListener("touchstart",touch_start_uab);
 window.addEventListener("touchmove",touch_move_uab);
 window.addEventListener("touchend",touch_end_uab);
 var canvas=document.getElementById("id_canvas");
 var context=canvas.getContext("2d");
+var canvas_rect=canvas.getBoundingClientRect();
 var last_position=[];
 function get_random_color()
 {
@@ -26,12 +27,12 @@ function touch_start_uab(p)
         touch_info.id=t[i].identifier;
 		touch_info.color=get_random_color();
 		context.beginPath();
-		context.arc(t[i].pageX ,t[i].pageY ,10,0,2*Math.PI);
+		context.arc(t[i].pageX-canvas_rect.left,t[i].pageY-canvas_rect.top ,10,0,2*Math.PI);
 		context.strokeStyle=touch_info.color;
 		context.fillStyle=touch_info.color;
 		context.lineWidth=1;
-		context.stroke();
 		context.fill();
+		context.stroke();
 		last_position.push(touch_info);
 	}
 }
@@ -49,13 +50,15 @@ function touch_move_uab(p)
 				break;
 			}
 			context.beginPath();
-			context.moveTo(last_position[index_t].x,last_position[index_t].y);
-			context.lineTo(t[i].pageX,t[i].pageY);
+			context.moveTo(last_position[index_t].x-canvas_rect.left,last_position[index_t].y-canvas_rect.top);
+			context.lineTo(t[i].pageX-canvas_rect.left,t[i].pageY-canvas_rect.top);
 			context.strokeStyle=last_position[index_t].color;
 		    context.fillStyle=last_position[index_t].color;
 			context.lineWidth=20;
-		    context.stroke();
 		    context.fill();
+			context.stroke();
+			last_position[index_t].x=t[i].pageX;
+			last_position[index_t].x=t[i].pageY;
 		}
     }
 	
