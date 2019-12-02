2a33e136c85dc43410255fa1d66092ff8fe9200f
catalinaboca <56110788+catalinaboca@users.noreply.github.com>
2019-12-02 11:18:54 +0200

    Add files via upload

--- touch/touch.js
+++ touch/touch.js
@@ -5,7 +5,7 @@ var context=canvas.getContext("2d");
 function touch_start_uab(p)
 {
 	var t=p.changedTouches;
-	for(int i=0;i<t.length;i++)
+	for(var i=0;i<t.length;i++)
 	{
 		context.beginPath();
 		context.arc(t[i].pageX ,t[i].pageY ,10,0,2*Math.PI);
