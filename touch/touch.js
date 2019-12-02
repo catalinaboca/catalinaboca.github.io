4d7821a24cf6cf41fb2bb4c237728fb1d246ad20
catalinaboca <56110788+catalinaboca@users.noreply.github.com>
2019-12-02 12:46:11 +0200

    Add files via upload

--- touch/touch.js
+++ touch/touch.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic").innerHTML="Logic 2019.12.02.5";
+document.getElementById("id_logic").innerHTML="Logic 2019.12.02.6";
 window.addEventListener("touchstart",touch_start_uab);
 window.addEventListener("touchmove",touch_move_uab,{passive:false});
 window.addEventListener("touchend",touch_end_uab);
@@ -39,30 +39,27 @@ function touch_start_uab(p)
 function touch_move_uab(p)
 {
 	p.preventDefault();
-	var t=p.changedTouches;
-	for(var i=0;i<t.length;i++)
-	{
-		var index_t=-1;
-		for(var j=0;j<last_position.length;j++)
-		{
-			if(last_position[j].id==t[i].identifier)
-			{
-				index_t=j;
+	
+	var t = p.changedTouches;
+	for (var i = 0; i < t.length; i++){
+		var index_t = -1;
+		for (var j = 0; j < last_position.length; j++)
+			if (last_position[j].id == t[i].identifier){
+				index_t = j;
 				break;
 			}
-			context.beginPath();
-			context.moveTo(last_position[index_t].x-canvas_rect.left,last_position[index_t].y-canvas_rect.top);
-			context.lineTo(t[i].pageX-canvas_rect.left,t[i].pageY-canvas_rect.top);
-			context.strokeStyle=last_position[index_t].color;
-		    context.fillStyle=last_position[index_t].color;
-			context.lineWidth=20;
-		    context.fill();
-			context.stroke();
-			last_position[index_t].x=t[i].pageX;
-			last_position[index_t].y=t[i].pageY;
-		}
-    }
-	
+		context.beginPath();
+		context.moveTo(last_position[index_t].x-canvas_rect.left, last_position[index_t].y-canvas_rect.top);
+		context.lineTo(t[i].pageX-canvas_rect.left, t[i].pageY - canvas_rect.top);
+		context.strokeStyle = last_position[index_t].color;
+		context.fillStyle = last_position[index_t].color;
+		context.lineWidth = 20;
+		context.fill();		
+		context.stroke();
+		
+		last_position[index_t].x = t[i].pageX;
+		last_position[index_t].y = t[i].pageY;
+	}
 }
 function touch_end_uab(p)
 {
