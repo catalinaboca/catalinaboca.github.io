6dcdda1c25061165d2a33c14a0255a0f9ce21d81
catalinaboca <56110788+catalinaboca@users.noreply.github.com>
2019-12-02 13:22:56 +0200

    Add files via upload

--- svg/svg.js
+++ svg/svg.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML="Logic=2019.12.02.1";
+document.getElementById("id_logic_version").innerHTML="Logic=2019.12.02.3";
 window.addEventListener("deviceorientation",on_gyro_data_uab);
 window.addEventListener("devicemotion",on_acc_data_uab);
 function deseneaza(unghi_x, unghi_y)
@@ -6,14 +6,14 @@ function deseneaza(unghi_x, unghi_y)
 	//obtinem context grafic 
 	var circle=document.getElementById("id_circle");
 	var svg=document.getElementById("id_svg");
-	var=circle.getAttribute("r");
+	var r=circle.getAttribute("r");
 	//adaugam un cerc la cale
-	var x=unghi_x/90 * (svg.width/2-r)+svg.width/2;
-	var y=unghi_x/90 * (svg.height/2-r)+svg.height/2;
-	context.arc(x,y,r,0,2*Math.PI);
+	var x=unghi_x/90 * (svg.width.animVal.value/2-r)+svg.width.animVal.value/2;
+	var y=unghi_x/90 * (svg.height.animVal.value/2-r)+svg.height.animVal.value/2;
+	//context.arc(x,y,r,0,2*Math.PI);
 	//actualizam pozitia cercului
-	circle.setAttribute("cx" ,x);
-	circle.setAttribute("cy" ,y);
+	circle.getAttribute("cx" ,x);
+	circle.getAttribute("cy" ,y);
 	
 }
 function on_gyro_data_uab(e)
