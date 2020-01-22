c1c8522a9f8b7ff5aa03371a3933aa951436c32c
catalinaboca <56110788+catalinaboca@users.noreply.github.com>
2020-01-22 16:03:48 +0200

    Add files via upload

--- Final_Project/view_app.js
+++ Final_Project/view_app.js
@@ -30,7 +30,7 @@ class app_view
 	}
       get_menu_V()
 	  {
-       return setTimeout("window.open('Clasa_a_V_a.html')",1000);
+       return setTimeout("window.open('catalinaboca.github.io/Final_Project/Clasa_a_V_a.html')",1000);
       }
 	   get_menu_VI()
 	  {
