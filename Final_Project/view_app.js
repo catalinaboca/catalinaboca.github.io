0bde51339d15cbb775f2d136b5f13fcd9a1f25f2
catalinaboca <56110788+catalinaboca@users.noreply.github.com>
2020-02-03 10:55:54 +0200

    Add files via upload

--- Final_Project/view_app.js
+++ Final_Project/view_app.js
@@ -6,13 +6,17 @@ class app_view
 		document.getElementById("id_button_V").addEventListener("click",this.on_click_navigation_V.bind(this));
 		document.getElementById("id_button_VI").addEventListener("click",this.on_click_navigation_VI.bind(this));
 		document.getElementById("id_button_VII").addEventListener("click",this.on_click_navigation_VII.bind(this));
-		document.getElementById("id_button_VIII").addEventListener("click",this.on_click_navigation_VIII.bind(this));
+	    document.getElementById("id_button_VIII").addEventListener("click",this.on_click_navigation_VIII.bind(this));
 	}
 	set_events_list(events)
 	{
 		this.events=events;
 	}
-    function on_click_navigation_V()
+	/*	on_click_result()
+	{
+		this.events.call_event("calcul");
+	}*/
+    on_click_navigation_V()
 	{
 		this.events.call_event("navigation_app_V");
 	}
@@ -28,20 +32,49 @@ class app_view
 	{
 		this.events.call_event("navigation_app_VIII");
 	}
-     function get_menu_V()
+   get_menu_V()
 	  {
-      setTimeout("window.open('Clasa_a_V_a.html')",1000);
+      return setTimeout("window.open('Clasa_a_V_a.html')",1000);
       }
 	   get_menu_VI()
 	  {
-       return setTimeout("window.open('C:/Users/beaut/Desktop/html/Final_Project/Clasa_a_VI_a.html')",1000);
+       return setTimeout("window.open('Clasa_a_VI_a.html')",1000);
       }
 	   get_menu_VII()
 	  {
-       return setTimeout("window.open('C:/Users/beaut/Desktop/html/Final_Project/Clasa_a_VII_a.html')",1000);
+       return setTimeout("window.open('Clasa_a_VII_a.html')",1000);
       }
 	   get_menu_VIII()
 	  {
-       return setTimeout("window.open('C:/Users/beaut/Desktop/html/Final_Project/Clasa_a_VIII_a.html')",1000);
+       return setTimeout("window.open('Clasa_a_VIII_a.html')",1000);
       }
+	  }
+	class app_view_calc
+	{
+		events;
+	constructor()
+	{
+		document.getElementById("id_buton").addEventListener("click",this.on_click_result.bind(this));
+	}
+	set_events_list(events)
+	{
+		this.events=events;
+	}
+	on_click_result()
+	{
+		this.events.call_event("calcul");
+	}
+     get_operatie()
+	 {
+		 return document.getElementById("id_operatie").value;
+	 }
+	 get_a()
+	 {
+		 return document.getElementById("id_a").value;
+	 }
+	 get_b()
+	 {
+		 return document.getElementById("id_b").value;
+	 }
+
 }
